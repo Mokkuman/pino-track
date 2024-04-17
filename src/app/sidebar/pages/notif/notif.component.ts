5b80c71dce9819abdc343593d71c62fa4d1dfb1a
mcdin09 <mcdinbwakpys@Outlook.com>
2024-04-16 21:50:12 -0600

    Agregando funciones para eliminar las notificaciones (Aunque faltaria el backend íµ´)

--- src/app/sidebar/pages/notif/notif.component.ts
+++ src/app/sidebar/pages/notif/notif.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-notif',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './notif.component.html',
   styleUrl: './notif.component.css'
 })
 export class NotifComponent {
+  display = false;
 
+  //Funcion para backend
+  checkValue(event: any){
+    console.log(event.currentTarget.id); //id de checkbutton para back 🥴
+    console.log(event.currentTarget.checked); //isChecked ?
+  }
 }
